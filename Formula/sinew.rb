6979c31df96d6011b6794851244eca26648bf53f
Kevin <dungle-scrubs@users.noreply.github.com>
2026-02-13 09:35:54 +0700

    sinew v0.2.0

--- Formula/sinew.rb
+++ Formula/sinew.rb
@@ -1,8 +1,8 @@
 class Sinew < Formula
   desc "macOS menu bar replacement with notch-aware layouts and hot-reload config"
   homepage "https://github.com/dungle-scrubs/sinew"
-  url "https://github.com/dungle-scrubs/sinew/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "2db236c5c84c2e627db738cd2d518dd7bece99cd51008d5272fc3a5727fc4a82"
+  url "https://github.com/dungle-scrubs/sinew/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "984cb9821370977893ca60c14cc9520877ec9a628876862e93d75591e60180b1"
   license "MIT"
 
   depends_on :macos
